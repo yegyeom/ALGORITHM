396fc07b8d0b0541e840cbef4c60814605c52924
yegyeom <reallyy1111@gmail.com>
2021-03-10 16:49:42 +0900

    13회차 [2021-03-08]

---
+++ BOJ/2529.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MAX 10
+using namespace std;
+
+int k;
+int num[MAX] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+vector <int> maxNum, minNum;
+char arr[9];
+
+int check(vector<int> &v){
+    for(int i = 0 ; i < k ; i++){
+        if(arr[i] == '<' && v[i] > v[i+1]) return 1;
+        else if(arr[i] == '>' && v[i] < v[i+1]) return 1;
+    }
+    return 0;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> k;
+
+    for(int i = 0 ; i < k ; i++){
+        cin >> arr[i];
+    }
+
+//max
+    for(int i = 9 ; i >= 9-k ; i--){
+        maxNum.push_back(i);
+    }
+
+    while(1){
+        if(check(maxNum)){
+            prev_permutation(maxNum.begin(), maxNum.end());
+        }
+        else break;
+    }
+
+//min
+    for(int i = 0 ; i <= k ; i++){
+        minNum.push_back(i);
+    }
+
+    while(1){
+        if(check(minNum)){
+            next_permutation(minNum.begin(), minNum.end());
+        }
+        else break;
+    }
+
+    for(int i = 0 ; i < k+1 ; i++)
+        cout << maxNum[i];
+    cout << "\n";
+
+    for(int i = 0 ; i < k+1 ; i++)
+        cout << minNum[i];
+    cout << "\n";
+
+    return 0;
+}
---
+++ BOJ/2623.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, m;
+    cin >> n >> m;
+
+    int inDegree[n+1] = {0,}, result[n+1];
+    vector<int> a[n+1];
+    queue<int> q;
+
+    for(int i = 0 ; i < m ; i++){
+        int num; cin >> num;
+        int arr[num];
+        for(int j = 0 ; j < num ; j++){
+            cin >> arr[j];
+        }
+        for(int k = 1 ; k < num ; k++){
+            a[arr[k-1]].push_back(arr[k]);
+            inDegree[arr[k]]++;
+        }
+    }
+
+    for(int i = 1 ; i <= n ; i++){
+        if(inDegree[i] == 0) q.push(i);
+    }
+
+    for(int i = 1 ; i <= n ; i++){
+        if(q.empty()){
+            cout << 0;
+            return 0;
+        }
+        int front = q.front();
+        q.pop();
+        result[i] = front;
+        for(int i = 0 ; i < a[front].size() ; i++){
+            int next = a[front][i];
+            if(--inDegree[next] == 0) 
+                q.push(next);
+        }
+    }
+//
+    for(int i = 1 ; i <= n ; i++){
+        cout << result[i] << "\n";
+    }
+
+    return 0;
+}
