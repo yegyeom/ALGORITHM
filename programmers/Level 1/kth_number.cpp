6f7b70d18362f2f40020b136e577e9a843ee27d0
yegyeom <reallyy1111@gmail.com>
2021-04-28 23:20:21 +0900

    solve problem

---
+++ programmers/Level 1/kth_number.cpp
@@ -0,0 +1,28 @@
+//programmers Level1: k번째수
+//정렬
+//2021-04-28
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+   
+    for(int i = 0 ; i < commands.size() ; i++){
+        vector<int> tmp;
+   
+        int c1 = commands[i][0];
+        int c2 = commands[i][1];
+        int c3 = commands[i][2];
+        
+        tmp.clear();
+        tmp.assign(array.begin() + (c1-1), array.begin() + c2);
+        
+        sort(tmp.begin(), tmp.end());
+        answer.push_back(tmp[c3 - 1]);
+    }
+    
+    return answer;
+}
