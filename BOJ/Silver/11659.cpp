86954745252a564858d7db5c2069b0c886183d07
yegyeom <reallyy1111@gmail.com>
2022-01-21 16:30:30 +0900

    [2022-01-21] 문제 풀이
    BOJ 11659

---
+++ BOJ/Silver/11659.cpp
@@ -0,0 +1,25 @@
+/*
+BOJ 11659번: 구간 합 구하기 4
+2022-01-21
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, m;
+    int a, b;
+    cin >> n >> m;
+
+    int arr[n + 1] = {0, };
+
+    for(int i = 1 ; i <= n ; i++) cin >> arr[i];
+    for(int i = 1 ; i <= n ; i++) arr[i] = arr[i - 1] + arr[i];
+
+    for(int i = 0 ; i < m ; i++) {
+        cin >> a >> b;
+        cout << arr[b] - arr[a - 1] << '\n';
+    }
+
+    return 0;
+}
