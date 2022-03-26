7160ef21e05872c8498ea64c068b5d1ced42af03
yegyeom <reallyy1111@gmail.com>
2022-03-26 20:15:56 +0900

    [2022-03-26] 문제 풀이
    programmers 단체사진 찍기

--- programmers/Level 2/take_a_group_photo.cpp
+++ programmers/Level 2/take_a_group_photo.cpp
@@ -1,112 +1,48 @@
-//programmers Level 2 : Take a group photo
-//2017 KAKAO CODE
-//2021-05-24
-#include <string>
+/*
+programmers Level 2: Îã®Ï≤¥ÏÇ¨ÏßÑ Ï∞çÍ∏∞
+2017 KAKAO CODE
+DATE: 2021-05-24
+UPDATE: 2022-03-26
+*/
 #include <vector>
 #include <algorithm>
-#include <iostream>
-
+#include <string>
 using namespace std;
 
-// ¿¸ø™ ∫Øºˆ∏¶ ¡§¿««“ ∞ÊøÏ «‘ºˆ ≥ªø° √ ±‚»≠ ƒ⁄µÂ∏¶ ≤¿ ¿€º∫«ÿ¡÷ººø‰.
 int solution(int n, vector<string> data) {
     int answer = 0;
-    vector<int> v;
-    vector<vector<int>> c;
-    
-    for(int i = 0 ; i < 8 ; i++){
-        v.push_back(i+1);
-    }
-
-    for(int i = 0 ; i < n ; i++){
-        vector<int> tmp;
-        int num;
-
-        if(data[i][0] == 'A' || data[i][2] == 'A'){
-            num = 1;
-            tmp.push_back(num);
-        }
-        if(data[i][0] == 'C' || data[i][2] == 'C'){
-            num = 2;
-            tmp.push_back(num);
-        }
-        if(data[i][0] == 'F' || data[i][2] == 'F'){
-            num = 3;
-            tmp.push_back(num);
-        }
-        if(data[i][0] == 'J' || data[i][2] == 'J'){
-            num = 4;
-            tmp.push_back(num);
-        }
-        if(data[i][0] == 'M' || data[i][2] == 'M'){
-            num = 5;
-            tmp.push_back(num);
-        }
-        if(data[i][0] == 'N' || data[i][2] == 'N'){
-            num = 6;
-            tmp.push_back(num);
-        }
-        if(data[i][0] == 'R' || data[i][2] == 'R'){
-            num = 7;
-            tmp.push_back(num);
-        }
-        if(data[i][0] == 'T' || data[i][2] == 'T'){
-            num = 8;
-            tmp.push_back(num);
-        }
-        
-        if(data[i][3] == '='){
-            tmp.push_back(0);
-        }
-        else if(data[i][3] == '<'){
-            tmp.push_back(-1);
-        }
-        else if(data[i][3] == '>'){
-            tmp.push_back(1);
-        }
-        
-        tmp.push_back(data[i][4] - '0');
-        c.push_back(tmp);
-    }
+    string str = "ACFJMNRT";
 
     do{
-        int cnt=0;
+        bool flag = true;
         
-        for(int i = 0 ; i < c.size() ; i++){
-            int num1 = find(v.begin(), v.end(), c[i][0]) - v.begin();
-            int num2 = find(v.begin(), v.end(), c[i][1]) - v.begin();
-
-            if(c[i][2] == 0){ // =
-                if(abs(num1 - num2) == c[i][3] + 1)
-                    cnt++;
-                else continue;
+        for(int i = 0 ; i < data.size() ; i++){
+            char ch1 = data[i][0];
+            char ch2 = data[i][2];
+            
+            int idx1 = find(str.begin(), str.end(), ch1) - str.begin();
+            int idx2 = find(str.begin(), str.end(), ch2) - str.begin();
+            
+            int diff = abs(idx1 - idx2) - 1;
+            int value = data[i][4] - '0';
+            
+            if(data[i][3] == '=' && diff != value){
+                flag = false;
+                break;
             }
-            else if(c[i][2] == -1){ // <
-                if(abs(num1 - num2) <= c[i][3])
-                    cnt++;
-                else continue;
+            else if(data[i][3] == '>' && diff <= value){
+                flag = false;
+                break;
             }
-            else if(c[i][2] == 1){ // >
-                if(abs(num1 - num2) - 1 > c[i][3])
-                    cnt++;
-                else continue;
+            else if(data[i][3] == '<' && diff >= value){
+                flag = false;
+                break;
             }
         }
-
-        if(cnt == n)
-            answer++;
-            
+        
+        if(flag) answer++;
     }
-    while(next_permutation(v.begin(), v.end()));
+    while(next_permutation(str.begin(), str.end()));
     
     return answer;
-}
-
-int main(){
-    vector<string> data = {"N~F=0", "R~T>2"};
-    int answer = solution(2, data);
-
-    cout << answer;
-
-    return 0;
 }
